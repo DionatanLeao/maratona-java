215bac054c6a050dc92ec202d64eccb476993f78
DionatanLeao <dionatan.ti@outlook.com>
2021-10-09 18:05:13 -0300

    Comments

--- src/academy/devdojo/maratonajava/introduction/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introduction/OlaDevDojo.java
@@ -1,10 +1,16 @@
 package academy.devdojo.maratonajava.introduction;
 
-/***
- * @author Dionatan Leão
+/**
+ * Isso é um comentário javadoc
  */
-
 public class OlaDevDojo {
+
+    // Isso é um comentário em linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas
+     */
     public static void main(String[] args) {
         System.out.println("Iniciando a Maratona Java");
     }
