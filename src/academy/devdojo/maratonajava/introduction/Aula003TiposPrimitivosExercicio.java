1289f653e08027df354ba06b75892cc8fb6c91bd
DionatanLeao <dionatan.ti@outlook.com>
2021-10-12 20:57:46 -0300

    Primitive Types en 05 - Exercise

---
+++ src/academy/devdojo/maratonajava/introduction/Aula003TiposPrimitivosExercicio.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula003TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Kirito";
+        String endereco = "Av Joao das galileias";
+        double salario = 5431.12;
+        String dataRecebimento = "20/12/2021";
+        String relatorio = "Eu " + nome + ", morando no endereço " + endereco +
+                ", confirmo que recebi o salário de " + salario + ", na data " + dataRecebimento;
+
+        System.out.println(relatorio);
+    }
+}
