2ccc28500a26f3dbeae5c8d3c82a09167027808c
DionatanLeao <dionatan.ti@outlook.com>
2021-10-12 20:25:21 -0300

    Primitive types pt 02 - in-memory declaration and size

--- src/academy/devdojo/maratonajava/introduction/Aula002TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introduction/Aula002TiposPrimitivos.java
@@ -7,6 +7,16 @@ public class Aula002TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, short, long, boolean
         int idade = 10;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
         System.out.println("A idade é " + idade + " anos");
+        System.out.println(verdadeiro);
+        System.out.println("char: " + caractere);
     }
 }
