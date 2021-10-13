7abcc758f24c30297c89b4a12e4fe6da7cf13606
DionatanLeao <dionatan.ti@outlook.com>
2021-10-12 21:08:15 -0300

    Operators pt 01 - Arithmetic

---
+++ src/academy/devdojo/maratonajava/introduction/Aula004Operadores.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula004Operadores {
+    public static void main(String[] args) {
+        //+-/*
+        int numero01 = 10;
+        int numero02 = 20;
+
+        int soma = numero01 + numero02;
+        int subtracao = numero01 - numero02;
+        int multiplicacao = numero01 * numero02;
+        double divisao = numero01 / numero02;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
