3ef6f1ee3c6c74b1ad32b99d369f0b07eea6eed0
DionatanLeao <dionatan.ti@outlook.com>
2021-10-19 20:13:25 -0300

    Operators en 05 - Assignment

--- src/academy/devdojo/maratonajava/introduction/Aula004Operadores.java
+++ src/academy/devdojo/maratonajava/introduction/Aula004Operadores.java
@@ -48,5 +48,26 @@ public class Aula004Operadores {
 
         boolean isPlayStationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlayStationCincoCompravel: " + isPlayStationCincoCompravel);
+
+        // =, +=, -=, *=, /=, %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /=  2;
+        bonus %= 2;
+        System.out.println("Bonus: " + bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println("Contador: " + contador);
+
+        int contador2 = 0;
+        System.out.println("Contador 2: " + ++contador2);
     }
 }
