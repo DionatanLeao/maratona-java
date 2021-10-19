a4e49d6923395f6bfd78cc81fe48a0cb3faa3c97
DionatanLeao <dionatan.ti@outlook.com>
2021-10-19 20:53:19 -0300

    Conditional Structures pt 01 - IF

---
+++ src/academy/devdojo/maratonajava/introduction/Aula005EstruturasCondicionais.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula005EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcoólica");
+        }
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcoólica");
+        }
+    }
+}
