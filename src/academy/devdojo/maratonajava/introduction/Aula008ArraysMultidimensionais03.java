c0bb81e964455cf28fd54bf3d706f0dc01bde527
DionatanLeao <dionatan.ti@outlook.com>
2021-11-21 10:45:03 -0300

    Multidimensional Arrays en 03 - Initialization

---
+++ src/academy/devdojo/maratonajava/introduction/Aula008ArraysMultidimensionais03.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula008ArraysMultidimensionais03 {
+    public static void main(String[] args) {
+        int[] arrays = {1,2,3};
+
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = arrays;
+        arrayInt[2] = new int[]{1,2,3,4,5,6};
+
+        for (int[] array: arrayInt) {
+            System.out.println("\n-----");
+            for (int integer : array) {
+                System.out.print(integer + " ");
+            }
+        }
+        System.out.println();
+        String[][] arrayNomes = {{"Mario","Maria"},{"Julio","Alberto","Matheus"}};
+
+        for (String[] arrString : arrayNomes) {
+            for (String nome: arrString) {
+                System.out.println(nome);
+            }
+        }
+    }
+}
