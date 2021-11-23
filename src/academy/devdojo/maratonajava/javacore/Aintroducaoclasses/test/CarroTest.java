116c2a324f5c98490014c23f9eafa6486ea02452
DionatanLeao <dionatan.ti@outlook.com>
2021-11-23 20:18:02 -0300

    Objects Orientation - Exercise Classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.modelo = "Sedan";
+        carro1.nome = "Monza";
+        carro1.ano = 1992;
+
+        Carro carro2 = new Carro();
+        carro2.modelo = "Sedan";
+        carro2.nome = "Santana";
+        carro2.ano = 1994;
+
+        System.out.println("Carro 1: " + carro1.nome + " modelo: " + carro1.modelo + " ano: " + carro1.ano);
+        System.out.println("Carro 2: " + carro2.nome + " modelo: " + carro2.modelo + " ano: " + carro2.ano);
+    }
+}
