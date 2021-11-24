5eb92eaf11bf96aa3fd670651d088786735515f8
DionatanLeao <dionatan.ti@outlook.com>
2021-11-24 20:29:25 -0300

    Orientation Objects - Methods en 02 - Parameters

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisnumeros(10,20.5F);
+    }
+}
