d3b7a4197b91f3492ec60711223080a81bcb2ccf
DionatanLeao <dionatan.ti@outlook.com>
2021-11-26 19:42:36 -0300

    Orientation Objects - Methods en 06 - Reference type parameters

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        ImpressoraEstudante print = new ImpressoraEstudante();
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+
+        estudante1.nome = "Midoriya";
+        estudante1.idade = 15;
+        estudante1.sexo =  'M';
+
+        estudante2.nome = "Sakura";
+        estudante2.idade = 16;
+        estudante2.sexo =  'F';
+
+        print.imprime(estudante1);
+        print.imprime(estudante2);
+
+
+
+    }
+}
