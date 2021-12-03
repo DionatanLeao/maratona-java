32989d7453fe788d5094b27d939072c5641bb5f5
DionatanLeao <dionatan.ti@outlook.com>
2021-12-02 21:59:36 -0300

    Association en 01 - Arrays with Objects

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador j1 = new Jogador("Pelé");
+        Jogador j2 = new Jogador("Romário");
+        Jogador j3 = new Jogador("Ronaldo");
+
+        Jogador[] jogadores = {j1, j2, j3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+    }
+}
