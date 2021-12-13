58613a3e7e60836bd773d8211b26b68b9d4f3fb5
DionatanLeao <dionatan.ti@outlook.com>
2021-12-12 22:07:49 -0300

    Orientation Objects - Final Modifier pt 01 - Primitive Type

---
+++ src/academy/devdojo/maratonajava/javacore/JmodificadorFinal/dominio/Carro.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.JmodificadorFinal.dominio;
+
+public class Carro {
+    private String nome;
+    public final double VELOCIDADE_LIMITE;
+
+    public Carro() {
+        VELOCIDADE_LIMITE = 250;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
