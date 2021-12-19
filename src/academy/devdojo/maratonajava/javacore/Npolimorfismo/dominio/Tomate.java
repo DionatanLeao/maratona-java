101d19010be927236c004b1587ed9ce8a2d3bbd5
DionatanLeao <dionatan.ti@outlook.com>
2021-12-19 20:50:19 -0300

    Object Orientation - Polymorphism pt 04 - Cast and instanceof

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
 
 public class Tomate extends Produto {
     public static final double IMPOSTO_POR_CENTO = 0.06;
-    private String dataVelocidade;
+    private String dataValidade;
 
     public Tomate(String nome, double valor) {
         super(nome, valor);
@@ -14,11 +14,11 @@ public class Tomate extends Produto {
         return this.valor * IMPOSTO_POR_CENTO;
     }
 
-    public String getDataVelocidade() {
-        return dataVelocidade;
+    public String getDataValidade() {
+        return dataValidade;
     }
 
-    public void setDataVelocidade(String dataVelocidade) {
-        this.dataVelocidade = dataVelocidade;
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
