03451b9fa230eb0ec2fc5652ca60c5f937cb3c11
DionatanLeao <dionatan.ti@outlook.com>
2021-12-24 12:26:22 -0300

    Exceptions en 06 - Finally block

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RunTimeExceptionTest03.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+        System.out.println("------------------------------------------");
+        abreConexao2();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abriando arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }
+
+    private static void abreConexao2() {
+        try {
+            System.out.println("Abriando arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            throw new RuntimeException();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
