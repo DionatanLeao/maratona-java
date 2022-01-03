5ee151e3cb32cf4b4f15096702cc453c3af04154
DionatanLeao <dionatan.ti@outlook.com>
2022-01-03 18:31:39 -0300

    Utility Classes - LocalTime

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
@@ -1,30 +1,19 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.LocalTime;
 import java.time.temporal.ChronoField;
-import java.util.Calendar;
-import java.util.Date;
 
 public class LocalTimeTest01 {
     public static void main(String[] args) {
-        System.out.println(new Date());
-        System.out.println(Calendar.getInstance());
-        System.out.println(Month.JANUARY.getValue());
-        LocalDate date = LocalDate.of(2022, Month.JANUARY, 27);
-        LocalDate agora = LocalDate.now();
-        agora = agora.plusWeeks(4);
-        System.out.println(date.getYear());
-        System.out.println(date.getMonthValue());
-        System.out.println(date.getDayOfWeek());
-        System.out.println(date.getDayOfMonth());
-        System.out.println(date.lengthOfMonth());
-        System.out.println(date.isLeapYear());
-        System.out.println(date.get(ChronoField.YEAR));
-        System.out.println(date.get(ChronoField.DAY_OF_MONTH));
-        System.out.println(date);
-        System.out.println(agora);
-        System.out.println(LocalDate.MAX);
-        System.out.println(LocalDate.MIN);
+        LocalTime localTime = LocalTime.of(23, 32, 12);
+        LocalTime timeNow = localTime.now();
+        System.out.println(localTime);
+        System.out.println(timeNow);
+        System.out.println(timeNow.getHour());
+        System.out.println(timeNow.getMinute());
+        System.out.println(timeNow.getSecond());
+        System.out.println(timeNow.get(ChronoField.CLOCK_HOUR_OF_AMPM));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
     }
 }
