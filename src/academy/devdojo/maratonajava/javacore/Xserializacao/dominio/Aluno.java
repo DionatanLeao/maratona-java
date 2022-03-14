ada14ffd428a5a2bb6cf02e5a15ceba299ec2f0e
DionatanLeao <dionatan.ti@outlook.com>
2022-03-14 20:12:58 -0300

    Utility Classes - Serialization pt 02

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
@@ -1,17 +1,42 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.dominio;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Aluno implements Serializable {
+    private static final long serialVersionUID = 739959230178355186L;
     private Long id;
     private String nome;
-    private String password;
+    private transient  String password;
+    private static String NOME_ESCOLA = "DevDojo virado no jiraya";
+    private transient Turma turma;
 
     public Aluno(Long id, String nome, String password) {
         System.out.println("Dentro do construtor");
         this.id = id;
         this.nome = nome;
         this.password = password;
+    }
+
+    private void writeObject(ObjectOutputStream oos) {
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Long getId() {
@@ -38,12 +63,22 @@ public class Aluno implements Serializable {
         this.password = password;
     }
 
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
     @Override
     public String toString() {
         return "Aluno{" +
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
+                ", NOME_ESCOLA='" + NOME_ESCOLA + '\'' +
+                ", Turma='" + turma + '\'' +
                 '}';
     }
 }
