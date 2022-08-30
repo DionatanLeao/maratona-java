ba2c9ec64d0ad4a3c6f2e8119b152e6df2742be1
DionatanLeao <dionatan.ti@outlook.com>
2022-08-29 23:47:26 -0300

    Generics pt 04 - Generic Classes pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.CarroRentavelService;
+
+public class ClasseGenericaTest02 {
+    public static void main(String[] args) {
+        BarcoRentavelService service = new BarcoRentavelService();
+        Barco barco = service.buscarBarcosDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        service.retornarBarcoAlugado(barco);
+    }
+}
