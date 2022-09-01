57a33c1f57f658964f08fd4bfcb85bbae0420c15
DionatanLeao <dionatan.ti@outlook.com>
2022-08-31 21:45:43 -0300

    Generics en 06 - Generic Methods

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+//        new DoisAtributos<Cachorro, Gato, Carro>();
+        List<Barco> barcoList = criarArrauyComUmObjeto(new Barco("Canoa Marota"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrauyComUmObjeto(T t) {
+        return List.of(t);
+
+    }
+}
+
+//class DoisAtributos<T, X, I> {
+//
+//    private T t;
+//    private X x;
+//    private I i;
+//
+//    public DoisAtributos(T t, X x, I i) {
+//        this.t = t;
+//        this.x = x;
+//        this.i = i;
+//    }
+//}
