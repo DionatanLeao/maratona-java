899d161cc09afb1c623eaaa57acdbc9e93cca7f6
DionatanLeao <dionatan.ti@outlook.com>
2023-03-11 22:58:31 -0300

    Competition pt 03 - Conditions

--- src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ReentrantLockTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ReentrantLockTest01.java
@@ -1,5 +1,8 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockTest01 {
@@ -26,19 +29,23 @@ class Worker implements Runnable {
 
     @Override
     public void run() {
-        lock.lock();
+        // lock.lock();
         try {
-            if(lock.isHeldByCurrentThread()) {
-                System.out.printf(" Thread %s entrou em uma sessão crítica%n", name);
-                System.out.printf("%d Threads esperando na fila%n", lock.getQueueLength());
-                System.out.printf("Thread %s vai esperar 2s%n", name);
-                Thread.sleep(2000);
-                System.out.printf("Thread %s finalizou a espera%n", name);
+            lock.tryLock(2, TimeUnit.SECONDS);
+            if (lock.isHeldByCurrentThread()) {
+                System.out.printf("Thread %s pegou o LOCK%n", name);
             }
+            System.out.printf(" Thread %s entrou em uma sessão crítica%n", name);
+            System.out.printf("%d Threads esperando na fila%n", lock.getQueueLength());
+            System.out.printf("Thread %s vai esperar 2s%n", name);
+            Thread.sleep(2000);
+            System.out.printf("Thread %s finalizou a espera%n", name);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
-            lock.unlock();
+            if(lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
         }
     }
 }
