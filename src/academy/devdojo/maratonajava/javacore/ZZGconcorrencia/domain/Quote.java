cc3ade9ce9303e2d006f689e157d43030a51c9fc
DionatanLeao <dionatan.ti@outlook.com>
2023-07-19 14:01:40 -0300

    Concurrency pt 15 - CompleteFuture pt 04 - Chaining calls pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/domain/Quote.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.domain;
+
+public final class Quote {
+    private final String store;
+    private final double price;
+    private final Discount.Code discountCode;
+
+    public Quote(String store, double price, Discount.Code discountCode) {
+        this.store = store;
+        this.price = price;
+        this.discountCode = discountCode;
+    }
+
+    public static Quote newQuote(String value) {
+        String[] values = value.split(":");
+        return new Quote(values[0], Double.parseDouble(values[1].replace(',', '.')), Discount.Code.valueOf(values[2]));
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Discount.Code getDiscountCode() {
+        return discountCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "store='" + store + '\'' +
+                ", price=" + price +
+                ", discountCode=" + discountCode +
+                '}';
+    }
+}
